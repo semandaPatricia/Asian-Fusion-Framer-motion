0d70e3e72ce7b27d08662f1214391e61dcd1aebc
semanda <patash248@gmail.com>
2024-03-19 12:46:41 +0100

    create Animate presense

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const containerVariants = {
-  hidden: { 
-    opacity: 0, 
-    x: '100vw',
+  hidden: {
+    opacity: 0,
+    x: "100vw",
     transition: {
       staggerChildren: 0.5,
-    } 
+    },
   },
-  visible: { 
-    opacity: 1, 
+  visible: {
+    opacity: 1,
     x: 0,
-    transition: { 
-      type: 'spring',
+    transition: {
+      type: "spring",
       mass: 0.4,
       damping: 8,
       staggerChildren: 0.4,
       when: "beforeChildren",
-    }
+    },
   },
 };
 
@@ -28,27 +28,33 @@ const childVariants = {
   },
   visible: {
     opacity: 1,
-  }
-}
-
-
-
-
+  },
+};
 
 const Order = ({ meal }) => {
+  const [showTitle, setShowTitle] = useState(true);
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000);
+
   // Ensure meal object is initialized with a default value
-  const defaultMeal = { base: '', toppings: [], sauce: '' };
+  const defaultMeal = { base: "", toppings: [], sauce: "" };
   const { base, toppings, sauce } = meal || defaultMeal;
 
   return (
-    <motion.div className="container order"
-    variants={containerVariants}
-    initial="hidden"
-    animate="visible"
-    
+    <motion.div
+      className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
-      <h2>Thank you for your order.</h2>
-      <motion.p variants={childVariants}>You ordered {base}  with:</motion.p>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2  exit={{ y: -1000 }}>Thank you for your order.</motion.h2>
+        )}
+      </AnimatePresence>
+
+      <motion.p variants={childVariants}>You ordered {base} with:</motion.p>
       <ul>
         {toppings && toppings.length > 0 ? (
           toppings.map((topping, index) => (
@@ -58,11 +64,11 @@ const Order = ({ meal }) => {
           <li>No toppings selected</li>
         )}
       </ul>
-      <motion.p variants={childVariants}>Sauce: {sauce || 'No sauce selected'}</motion.p>
+      <motion.p variants={childVariants}>
+        Sauce: {sauce || "No sauce selected"}
+      </motion.p>
     </motion.div>
   );
 };
 
 export default Order;
-
-
